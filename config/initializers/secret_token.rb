1899f627c93bf69d2445abbed52be184cad02cad
Cyrus Kazemi <cyruskazemi@gmail.com>
2011-01-24 20:33:41 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Giveto::Application.config.secret_token = '555b14157885b972245523ccd5ef3ec4c918a492b27ea1bf680f485fc097a1a5a87192c6380d13e8807607a7b43d0c9b39d985594d79189390313ebbcc3b5d71'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Giveto::Application.config.session_store :cookie_store, :key => '_giveto_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Giveto::Application.config.session_store :active_record_store
