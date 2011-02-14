ef931a37bf701422f2fc0d95648483f721e48964
Cyrus Kazemi <cyruskazemi@gmail.com>
2011-02-13 19:58:33 -0800

    sign up error styling

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -31,12 +31,12 @@ describe UsersController do
   
   describe "GET 'new'" do
     it "should be successful" do
-      get 'new'
+      get :new
       response.should be_success
     end
     
     it "should have the right title" do
-      get 'new'
+      get :new
       response.should have_selector("title", :content => "Sign up")
     end
   end
