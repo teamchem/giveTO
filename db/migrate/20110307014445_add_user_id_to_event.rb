dc1f13bdbadb8f83811b532974050349bce9b8d3
Cyrus Kazemi <cyruskazemi@gmail.com>
2011-03-07 13:21:58 -0800

    added user_id to new event

---
+++ db/migrate/20110307014445_add_user_id_to_event.rb
@@ -0,0 +1,9 @@
+class AddUserIdToEvent < ActiveRecord::Migration
+  def self.up
+    add_column :events, :user_id, :integer
+  end
+
+  def self.down
+    remove_column :events, :user_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110304044356) do
+ActiveRecord::Schema.define(:version => 20110307014445) do
 
   create_table "events", :force => true do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20110304044356) do
     t.datetime "updated_at"
     t.string   "rso_type"
     t.string   "description"
+    t.integer  "user_id"
   end
 
   create_table "organizations", :force => true do |t|
